6b272b1bcf72ca8d1d2da08aa76ff01c83ebd47f
shashanksinghthakur <85666787+shashanksinghthakur@users.noreply.github.com>
2022-01-10 13:48:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("header").style.top = "0";
+/*window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80) {
+
+        document.getElementById("header").blur();
     } else {
-        document.getElementById("header").style.top = "-150px";
+
+        document.getElementById("header").style.background = "#a0b3c1";
     }
-    prevScrollpos = currentScrollPos;
+}*/
+
+
+var el = document.querySelectorAll(".rt-container ul > div");
+
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
 }
